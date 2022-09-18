709341dfc728ad736143cb1a1476119084bad84b
marcosbarrozo <104003734+marcosbarrozo@users.noreply.github.com>
2022-09-17 20:38:40 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 
+const links = [
+  {
+    label: "Week1 notes",
+    url: "week1/index.html"
+  }
+]
+function loadIndex (){
+  const ol = document.querySelector("#linksList");
+  links.forEach( link =>{
+    const link = document.createElement("li");
+    const href = document.createElement("a");
+    href.setAttribute("href", link.url);
+    href.innerText = link.label;
+    
+    link.appendChild(href);
+    ol.appendChild(link);
+  })
+}
